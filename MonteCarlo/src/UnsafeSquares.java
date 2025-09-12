6d98c9988c4f0ad9fa9b538a4226c7b64f511dfc
Sid Senthil <sidsenthil2010@gmail.com>
2025-09-12 09:54:39 -0700

    kljhgfhlgjvjhkb

--- MonteCarlo/src/UnsafeSquares.java
+++ MonteCarlo/src/UnsafeSquares.java
@@ -1,30 +1,30 @@
-public class UnsafeSquares {
-    public static void main(String[] args) {
-
-    }
-
-    public static int[][] createBoard(int numQueens) {
-        int[][] board = new int[8][8];
-        int numQueensPlaced = 0;
-        while(numQueensPlaced < numQueens) {
-            
-            int x = (int)(Math.random() * 8);
-            int y = (int)(Math.random() * 8);
-            
-            if(board[x][y] == 0) {
-                board[x][y] = 1;
-                numQueensPlaced++;
-            }
-            
-        } 
-    }
-
-    public static int numSquaresSafe(int numQueens) {
-        int[][] board = createBoard(numQueens);
-        
-        
-        
-    }
-
-
-}
+//public class UnsafeSquares {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static int[][] createBoard(int numQueens) {
+//        int[][] board = new int[8][8];
+//        int numQueensPlaced = 0;
+//        while(numQueensPlaced < numQueens) {
+//
+//            int x = (int)(Math.random() * 8);
+//            int y = (int)(Math.random() * 8);
+//
+//            if(board[x][y] == 0) {
+//                board[x][y] = 1;
+//                numQueensPlaced++;
+//            }
+//
+//        }
+//    }
+//
+//    public static int numSquaresSafe(int numQueens) {
+//        int[][] board = createBoard(numQueens);
+//
+//
+//
+//    }
+//
+//
+//}
