43bf5f28de97ce09d4537af718864352f80d5350
Sid Senthil <sidsenthil2010@gmail.com>
2026-03-11 09:57:46 -0700

    insertstars and check palindrome recursively

--- recursion/src/Main.java
+++ recursion/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        long answer = fact(4);
+        boolean answer = palindrome("abcba");
         System.out.println(answer);
     }
 
@@ -13,5 +13,19 @@ public class Main {
     private static long fact(int n) {
         if (n == 1) return 1;
         return fact(n-1) * n;
+    }
+
+    private static String insertstars(String str) {
+        if (str.length() == 1) return str;
+        if (str.length() == 2) return str.charAt(0) + "*" + str.charAt(1);
+        int middle = str.length() / 2;
+        return insertstars(str.substring(0, middle)) + "*" + insertstars(str.substring(middle));
+    }
+
+    private static boolean palindrome(String str) {
+        if (str.length() == 1) return true;
+        if (str.length() == 2) return (str.charAt(0) == str.charAt(1));
+        boolean startEnd = str.charAt(0) == str.charAt(str.length()-1);
+        return startEnd && palindrome(str.substring(1, str.length()-1));
     }
 }
